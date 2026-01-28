a65f30e63c495b9b26846e413aa3c1299b794133
Luong Gia Tuan <tuangb2004@gmail.com>
2026-01-28 20:47:20 +0700

    feat(icons): Refactor SVG icon imports to use bundled paths for production deployment

--- frontend/src/components/Icons.jsx
+++ frontend/src/components/Icons.jsx
@@ -1,6 +1,45 @@
 import React from 'react';
 import * as Lucide from 'lucide-react';
 
+// Import SVGs so Vite bundles them correctly in production (paths like /src/assets/... 404 on deploy)
+import logoSvg from '../assets/svg/Logo.svg';
+import bellSvg from '../assets/svg/bell-svgrepo-com.svg';
+import boxSvg from '../assets/svg/box-minimalistic-svgrepo-com.svg';
+import calendarSvg from '../assets/svg/calendar-add-svgrepo-com.svg';
+import clapperboardSvg from '../assets/svg/clapperboard-edit-svgrepo-com.svg';
+import clapperboardPlaySvg from '../assets/svg/clapperboard-play-svgrepo-com.svg';
+import clockSvg from '../assets/svg/clock-circle-svgrepo-com.svg';
+import copySvg from '../assets/svg/copy-svgrepo-com.svg';
+import downloadSvg from '../assets/svg/download-minimalistic-svgrepo-com.svg';
+import eyeSvg from '../assets/svg/eye-svgrepo-com.svg';
+import heartSvg from '../assets/svg/heart-svgrepo-com.svg';
+import layersSvg from '../assets/svg/layers-minimalistic-svgrepo-com.svg';
+import lightbulbSvg from '../assets/svg/lightbulb-bolt-svgrepo-com.svg';
+import linkSvg from '../assets/svg/link-svgrepo-com.svg';
+import logoutSvg from '../assets/svg/logout-2-svgrepo-com.svg';
+import micSvg from '../assets/svg/microphone-svgrepo-com.svg';
+import moonSvg from '../assets/svg/moon-svgrepo-com.svg';
+import notebookSvg from '../assets/svg/notebook-svgrepo-com.svg';
+import pieChartSvg from '../assets/svg/pie-chart-svgrepo-com.svg';
+import pinSvg from '../assets/svg/pin-svgrepo-com.svg';
+import pipSvg from '../assets/svg/pip-svgrepo-com.svg';
+import refreshSvg from '../assets/svg/refresh-svgrepo-com.svg';
+import shareSvg from '../assets/svg/share-svgrepo-com.svg';
+import slidersSvg from '../assets/svg/slider-vertical-svgrepo-com.svg';
+import sunSvg from '../assets/svg/sun-svgrepo-com.svg';
+import trashSvg from '../assets/svg/trash-bin-2-svgrepo-com.svg';
+import userSvg from '../assets/svg/user-id-svgrepo-com.svg';
+import filterSvg from '../assets/svg/filters-svgrepo-com.svg';
+import earthSvg from '../assets/svg/earth-svgrepo-com.svg';
+import galleryEditSvg from '../assets/svg/gallery-edit-svgrepo-com.svg';
+import gallerySvg from '../assets/svg/gallery-svgrepo-com.svg';
+import maximizeSvg from '../assets/svg/maximize-square-svgrepo-com.svg';
+import homeSvg from '../assets/svg/home-svgrepo-com.svg';
+import chartSquareSvg from '../assets/svg/chart-square-svgrepo-com.svg';
+import cropSvg from '../assets/svg/crop-minimalistic-svgrepo-com.svg';
+import magicStickSvg from '../assets/svg/magic-stick-3-svgrepo-com.svg';
+import stickerSmileSvg from '../assets/svg/sticker-smile-circle-2-svgrepo-com.svg';
+
 const SvgIcon = ({ src, size = 24, className = "" }) => {
     return (
         <img
@@ -21,47 +60,47 @@ const LucideIcon = ({ icon, size = 24, className = "" }) => {
 };
 
 export const Icons = {
-    // SVG Assets from assets/svg
-    Logo: (props) => <SvgIcon src="/src/assets/svg/Logo.svg" {...props} />,
-    Bell: (props) => <SvgIcon src="/src/assets/svg/bell-svgrepo-com.svg" {...props} />,
-    Box: (props) => <SvgIcon src="/src/assets/svg/box-minimalistic-svgrepo-com.svg" {...props} />,
-    Calendar: (props) => <SvgIcon src="/src/assets/svg/calendar-add-svgrepo-com.svg" {...props} />,
-    Clapperboard: (props) => <SvgIcon src="/src/assets/svg/clapperboard-edit-svgrepo-com.svg" {...props} />,
-    ClapperboardPlay: (props) => <SvgIcon src="/src/assets/svg/clapperboard-play-svgrepo-com.svg" {...props} />,
-    Clock: (props) => <SvgIcon src="/src/assets/svg/clock-circle-svgrepo-com.svg" {...props} />,
-    Copy: (props) => <SvgIcon src="/src/assets/svg/copy-svgrepo-com.svg" {...props} />,
-    Download: (props) => <SvgIcon src="/src/assets/svg/download-minimalistic-svgrepo-com.svg" {...props} />,
-    Eye: (props) => <SvgIcon src="/src/assets/svg/eye-svgrepo-com.svg" {...props} />,
-    Heart: (props) => <SvgIcon src="/src/assets/svg/heart-svgrepo-com.svg" {...props} />,
-    Layers: (props) => <SvgIcon src="/src/assets/svg/layers-minimalistic-svgrepo-com.svg" {...props} />,
-    Lightbulb: (props) => <SvgIcon src="/src/assets/svg/lightbulb-bolt-svgrepo-com.svg" {...props} />,
-    Link: (props) => <SvgIcon src="/src/assets/svg/link-svgrepo-com.svg" {...props} />,
-    LogOut: (props) => <SvgIcon src="/src/assets/svg/logout-2-svgrepo-com.svg" {...props} />,
-    Mic: (props) => <SvgIcon src="/src/assets/svg/microphone-svgrepo-com.svg" {...props} />,
-    Moon: (props) => <SvgIcon src="/src/assets/svg/moon-svgrepo-com.svg" {...props} />,
-    Notebook: (props) => <SvgIcon src="/src/assets/svg/notebook-svgrepo-com.svg" {...props} />,
-    PieChart: (props) => <SvgIcon src="/src/assets/svg/pie-chart-svgrepo-com.svg" {...props} />,
-    Pin: (props) => <SvgIcon src="/src/assets/svg/pin-svgrepo-com.svg" {...props} />,
-    Pip: (props) => <SvgIcon src="/src/assets/svg/pip-svgrepo-com.svg" {...props} />,
-    RefreshCw: (props) => <SvgIcon src="/src/assets/svg/refresh-svgrepo-com.svg" {...props} />,
-    Share2: (props) => <SvgIcon src="/src/assets/svg/share-svgrepo-com.svg" {...props} />,
-    Sliders: (props) => <SvgIcon src="/src/assets/svg/slider-vertical-svgrepo-com.svg" {...props} />,
-    Sun: (props) => <SvgIcon src="/src/assets/svg/sun-svgrepo-com.svg" {...props} />,
-    Trash2: (props) => <SvgIcon src="/src/assets/svg/trash-bin-2-svgrepo-com.svg" {...props} />,
-    User: (props) => <SvgIcon src="/src/assets/svg/user-id-svgrepo-com.svg" {...props} />,
-    Video: (props) => <SvgIcon src="/src/assets/svg/video-library-svgrepo-com.svg" {...props} />,
-    Filter: (props) => <SvgIcon src="/src/assets/svg/filters-svgrepo-com.svg" {...props} />,
-    Globe: (props) => <SvgIcon src="/src/assets/svg/earth-svgrepo-com.svg" {...props} />,
-    Image: (props) => <SvgIcon src="/src/assets/svg/gallery-edit-svgrepo-com.svg" {...props} />,
-    Gallery: (props) => <SvgIcon src="/src/assets/svg/gallery-svgrepo-com.svg" {...props} />,
-    Maximize2: (props) => <SvgIcon src="/src/assets/svg/maximize-square-svgrepo-com.svg" {...props} />,
-    Home: (props) => <SvgIcon src="/src/assets/svg/home-svgrepo-com.svg" {...props} />,
-    BarChart2: (props) => <SvgIcon src="/src/assets/svg/chart-square-svgrepo-com.svg" {...props} />,
-    Ratio: (props) => <SvgIcon src="/src/assets/svg/crop-minimalistic-svgrepo-com.svg" {...props} />,
-    Crop: (props) => <SvgIcon src="/src/assets/svg/crop-minimalistic-svgrepo-com.svg" {...props} />,
-    Wand2: (props) => <SvgIcon src="/src/assets/svg/magic-stick-3-svgrepo-com.svg" {...props} />,
-    Users: (props) => <SvgIcon src="/src/assets/svg/sticker-smile-circle-2-svgrepo-com.svg" {...props} />,
-    Cloud: (props) => <SvgIcon src="/src/assets/svg/layers-minimalistic-svgrepo-com.svg" {...props} />,
+    // SVG Assets – use imported URLs so production build serves correct paths
+    Logo: (props) => <SvgIcon src={logoSvg} {...props} />,
+    Bell: (props) => <SvgIcon src={bellSvg} {...props} />,
+    Box: (props) => <SvgIcon src={boxSvg} {...props} />,
+    Calendar: (props) => <SvgIcon src={calendarSvg} {...props} />,
+    Clapperboard: (props) => <SvgIcon src={clapperboardSvg} {...props} />,
+    ClapperboardPlay: (props) => <SvgIcon src={clapperboardPlaySvg} {...props} />,
+    Clock: (props) => <SvgIcon src={clockSvg} {...props} />,
+    Copy: (props) => <SvgIcon src={copySvg} {...props} />,
+    Download: (props) => <SvgIcon src={downloadSvg} {...props} />,
+    Eye: (props) => <SvgIcon src={eyeSvg} {...props} />,
+    Heart: (props) => <SvgIcon src={heartSvg} {...props} />,
+    Layers: (props) => <SvgIcon src={layersSvg} {...props} />,
+    Lightbulb: (props) => <SvgIcon src={lightbulbSvg} {...props} />,
+    Link: (props) => <SvgIcon src={linkSvg} {...props} />,
+    LogOut: (props) => <SvgIcon src={logoutSvg} {...props} />,
+    Mic: (props) => <SvgIcon src={micSvg} {...props} />,
+    Moon: (props) => <SvgIcon src={moonSvg} {...props} />,
+    Notebook: (props) => <SvgIcon src={notebookSvg} {...props} />,
+    PieChart: (props) => <SvgIcon src={pieChartSvg} {...props} />,
+    Pin: (props) => <SvgIcon src={pinSvg} {...props} />,
+    Pip: (props) => <SvgIcon src={pipSvg} {...props} />,
+    RefreshCw: (props) => <SvgIcon src={refreshSvg} {...props} />,
+    Share2: (props) => <SvgIcon src={shareSvg} {...props} />,
+    Sliders: (props) => <SvgIcon src={slidersSvg} {...props} />,
+    Sun: (props) => <SvgIcon src={sunSvg} {...props} />,
+    Trash2: (props) => <SvgIcon src={trashSvg} {...props} />,
+    User: (props) => <SvgIcon src={userSvg} {...props} />,
+    Video: (props) => <LucideIcon icon="Video" {...props} />,
+    Filter: (props) => <SvgIcon src={filterSvg} {...props} />,
+    Globe: (props) => <SvgIcon src={earthSvg} {...props} />,
+    Image: (props) => <SvgIcon src={galleryEditSvg} {...props} />,
+    Gallery: (props) => <SvgIcon src={gallerySvg} {...props} />,
+    Maximize2: (props) => <SvgIcon src={maximizeSvg} {...props} />,
+    Home: (props) => <SvgIcon src={homeSvg} {...props} />,
+    BarChart2: (props) => <SvgIcon src={chartSquareSvg} {...props} />,
+    Ratio: (props) => <SvgIcon src={cropSvg} {...props} />,
+    Crop: (props) => <SvgIcon src={cropSvg} {...props} />,
+    Wand2: (props) => <SvgIcon src={magicStickSvg} {...props} />,
+    Users: (props) => <SvgIcon src={stickerSmileSvg} {...props} />,
+    Cloud: (props) => <SvgIcon src={layersSvg} {...props} />,
 
     // Lucide Fallbacks
     LayoutGrid: (props) => <LucideIcon icon="LayoutGrid" {...props} />,
